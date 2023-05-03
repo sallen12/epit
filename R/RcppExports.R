1574bbb7b8181640e23aaf507e7708fce80fd869
sallen12 <sam.allen@stat.unibe.ch>
2023-05-03 17:26:38 +0200

    correct documentation of e_rank_histogram

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -188,8 +188,7 @@ sequential_grenander <- function(z, pos_Z) {
 #' \code{1,...,m}.
 #'
 #' @param r observations.
-#' @param m size of discrete uniform distribution PLUS ONE (positive integer,
-#'     greater or equal to \code{max(r)+1}).
+#' @param m size of discrete uniform distribution.
 #' @param n0 minimum number of observations for starting. All e-values until
 #'     \code{n0} (included) are equal to 1.
 #' @param ... currently not used, only for calling the function with
